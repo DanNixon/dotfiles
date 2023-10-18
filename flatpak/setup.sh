0fba210259100c7ab8cd22bcc4ff738d9e4dbb75
Dan Nixon <dan@dan-nixon.com>
2023-10-18 19:30:51 +0100

    Add note in Flatpak setup script
    [ci skip]

--- flatpak/setup.sh
+++ flatpak/setup.sh
@@ -5,5 +5,6 @@ set -exo pipefail
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
 
+# Make system fonts available
 flatpak --user override --filesystem=$HOME/.local/share/fonts
 flatpak --user override --filesystem=/nix/store
