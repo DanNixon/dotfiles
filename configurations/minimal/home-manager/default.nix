a49f92baa726b5ab35c2186eac60e5673a5feeb7
Dan Nixon <dan@dan-nixon.com>
2023-12-24 14:50:08 +0000

    Add minimal Home Manager config

---
+++ configurations/minimal/home-manager/default.nix
@@ -0,0 +1,43 @@
+{
+  inputs,
+  outputs,
+}: let
+  pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
+in
+  inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    extraSpecialArgs = {inherit inputs outputs pkgs-unstable;};
+
+    modules = [
+      inputs.base16.homeManagerModule
+      ../../../modules/home-manager/base.nix
+      ../../../modules/home-manager/terminal_environment/git.nix
+      ../../../modules/home-manager/terminal_environment/neovim
+      ../../../modules/home-manager/terminal_environment/shell.nix
+      ../../../modules/home-manager/terminal_environment/tmux.nix
+      ../../../modules/home-manager/terminal_environment/zsh.nix
+
+      ({pkgs, ...}: {
+        home.packages = with pkgs; [
+          # System tools
+          bottom
+          lm_sensors
+          neofetch
+          progress
+
+          # Disk/filesystem tools
+          dua
+          smartmontools
+          tree
+
+          # Networking tools
+          bmon
+        ];
+
+        # home = {
+        #   username = "dan";
+        #   homeDirectory = "/home/dan";
+        # };
+      })
+    ];
+  }
