d2275f2972edcfcb740184b8829b1b5e3b6569c0
Dan Nixon <dan@dan-nixon.com>
2024-08-28 11:21:16 +0100

    nix-index: use fzf for comma picker

--- modules/home-manager/terminal_environment/nix-index.nix
+++ modules/home-manager/terminal_environment/nix-index.nix
@@ -1,12 +1,17 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.nix-index-database.hmModules.nix-index
   ];
-
-  programs.nix-index-database.comma.enable = true;
 
   programs.nix-index = {
     enableZshIntegration = false;
     enableBashIntegration = false;
   };
+
+  programs.nix-index-database.comma.enable = true;
+  home.sessionVariables.COMMA_PICKER = "${pkgs.fzf}/bin/fzf";
 }
