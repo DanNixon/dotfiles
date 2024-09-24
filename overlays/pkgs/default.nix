59c3424dbcc2335dbca0a68c1e21aebf5bf4eb57
Dan Nixon <dan@dan-nixon.com>
2024-09-24 07:06:01 +0100

    desktop environment: remove firefox-private

--- overlays/pkgs/default.nix
+++ overlays/pkgs/default.nix
@@ -1,7 +1,6 @@
 {pkgs}: {
   ddcutil-modify-vcp = pkgs.callPackage ./ddcutil-modify-vcp {};
   de-screenshot = pkgs.callPackage ./de-screenshot {};
-  firefox-private = pkgs.callPackage ./firefox-private {};
   media-control = pkgs.callPackage ./media-control {};
   sway-helper = pkgs.callPackage ./sway-helper {};
 }
