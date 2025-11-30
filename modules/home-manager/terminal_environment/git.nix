033d81bea3c781cf0fd44c0e88a2ffb0d105bd45
Dan Nixon <dan@dan-nixon.com>
2025-11-30 12:51:53 +0000

    Move Git config to plain files

--- modules/home-manager/terminal_environment/git.nix
+++ modules/home-manager/terminal_environment/git.nix
@@ -5,113 +5,15 @@
     lg = "lazygit";
   };
 
-  programs.git = {
-    enable = true;
-
-    userName = "Dan Nixon";
-    userEmail = "dan@dan-nixon.com";
-
-    aliases = {
-      ap = "add --patch";
-
-      s = "status --short --branch";
-      ss = "show";
-
-      co = "checkout";
-      sw = "switch";
-
-      c = "commit";
-      ca = "commit --amend";
-      cf = "commit --fixup";
-
-      d = "diff";
-      ds = "diff --staged";
-
-      b = "branch --all";
-      bd = "branch -D";
-
-      l = "log --pretty=concise";
-      la = "log --pretty=full --graph --show-signature";
-      lg = "log --pretty=concise --graph";
-      lb = "log --pretty=cleangraph --graph --branches --simplify-by-decoration";
-      rl = "log --pretty=reflog --walk-reflogs";
-
-      rb = "rebase";
-      rbi = "rebase --interactive";
-      rbc = "rebase --continue";
-
-      p = "push";
-      pu = "push --set-upstream";
-      pfl = "push --force-with-lease";
-
-      f = "fetch";
-
-      cla = "clean -ixd";
-
-      test-pr = "!f() { git fetch $1 pull/$2/merge:pr/$2-merged && git checkout pr/$2-merged; }; f";
-    };
-
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      push = {
-        default = "current";
-      };
-      pull = {
-        ff = "only";
-      };
-      fetch = {
-        prune = true;
-      };
-      rebase = {
-        autoSquash = true;
-      };
-      merge = {
-        conflictStyle = "diff3";
-        tool = "meld";
-      };
-      mergetool = {
-        prompt = false;
-      };
-      diff = {
-        colorMoved = "plain";
-        tool = "meld";
-      };
-      "diff \"bin\"" = {
-        textconv = "hexdump -v -C";
-      };
-      "diff \"zip\"" = {
-        textconv = "unzip -c -a";
-      };
-      difftool = {
-        prompt = false;
-      };
-      pretty = {
-        concise = "%C(yellow)%h %C(reset)%ad %C(blue)%an %C(reset)%s%C(green)%d";
-        cleangraph = "%C(yellow)%h %C(reset)%ad %C(reset)%s%C(green)%d";
-        reflog = "%C(yellow)%h %C(reset)%ad %C(blue)%an %C(reset)%gs%C(green)%d";
-      };
-    };
-
-    attributes = [
-      # FreeCad project file
-      "*.FCStd diff=zip"
-    ];
-
-    ignores = [
-      # Tomb
-      ".host"
-      ".last"
-      ".tty"
-      ".uid"
-    ];
-  };
-
-  programs.lazygit.enable = true;
-
   home.packages = with pkgs; [
     gfold
+    git
+    lazygit
     jujutsu
   ];
+
+  xdg.configFile = {
+    "git/attributes".source = ./gitattributes.txt;
+    "git/config".source = ./gitconfig.txt;
+  };
 }
