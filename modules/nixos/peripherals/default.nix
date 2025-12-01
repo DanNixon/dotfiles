12a95ab5d0728eca5eaaf0d41c964c80a2b47fd1
Dan Nixon <dan@dan-nixon.com>
2025-12-01 20:22:25 +0000

    Make Pi debug probe work again
    This should not be needed.

--- modules/nixos/peripherals/default.nix
+++ modules/nixos/peripherals/default.nix
@@ -16,5 +16,7 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0922", ATTRS{idProduct}=="1001", MODE="0666"
     ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0922", ATTRS{idProduct}=="0015", MODE="0666"
+
+    ATTRS{product}=="*CMSIS-DAP*", MODE="0666"
   '';
 }
