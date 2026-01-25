9c0df4f9bfa798baff3a58adfce76881e829e98f
Dan Nixon <dan@dan-nixon.com>
2026-01-25 21:07:06 +0000

    Add backup config for hina

--- configurations/hina/home-manager/default.nix
+++ configurations/hina/home-manager/default.nix
@@ -8,5 +8,25 @@ in
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     extraSpecialArgs = {inherit inputs outputs pkgs-unstable;};
 
-    modules = [../../../modules/home-manager];
+    modules = [
+      ../../../modules/home-manager
+      ../../../modules/home-manager/borgmatic.nix
+
+      ({...}: {
+        programs.borgmatic.backups."main" = {
+          location = {
+            sourceDirectories = [
+              "/home/dan/documents"
+              "/home/dan/notebook"
+              "/home/dan/notebook-old"
+              "/home/dan/.local/share/koishi-store"
+              "/home/dan/.local/share/password-store"
+              "/home/dan/phone"
+            ];
+            repositories = ["ssh://i3caziwg@i3caziwg.repo.borgbase.com/./repo"];
+          };
+          storage.encryptionPasscommand = "koishi get borg/hina.yaml passphrase";
+        };
+      })
+    ];
   }
