273c08a5ea49d48be5db0f98f25593269a9dfc24
Dan Nixon <dan@dan-nixon.com>
2026-09-11 08:30:39 +0100

    wip

--- configurations/hina/home-manager/default.nix
+++ configurations/hina/home-manager/default.nix
@@ -22,7 +22,7 @@ in
               "/home/dan/.local/share/password-store"
               "/home/dan/phone"
             ];
-            repositories = ["ssh://i3caziwg@i3caziwg.repo.borgbase.com/./repo"];
+            repositories = ["ssh://lkqv1sw2@lkqv1sw2.repo.borgbase.com/./repo"];
           };
           storage.encryptionPasscommand = "koishi get borg/hina.yaml passphrase";
         };
