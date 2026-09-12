c2e2cca301fdeb20bcac09ac75e694cedf4a617d
Dan Nixon <dan@dan-nixon.com>
2026-09-12 15:11:53 +0100

    wip

--- configurations/yama/home-manager/default.nix
+++ configurations/yama/home-manager/default.nix
@@ -22,7 +22,7 @@ in
               "/home/dan/.local/share/password-store"
               "/home/dan/phone"
             ];
-            repositories = ["ssh://j409s2e9@j409s2e9.repo.borgbase.com/./repo"];
+            repositories = ["ssh://o2dl3om5@o2dl3om5.repo.borgbase.com/./repo"];
           };
           storage.encryptionPasscommand = "koishi get borg/yama.yaml passphrase";
         };
